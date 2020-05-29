70aeb5c17f5a5532708614126d974290f9ddfc1b
Mudit Mittal <mmudit30@gmail.com>
2020-05-29 21:17:43 +0530

    log commented

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -5,7 +5,7 @@ import CountUp from 'react-countup';
 import cx from 'classnames';
 
 const Cards =({data : {confirmed, recovered, deaths, lastUpdate}}) => {
-    console.log(confirmed);
+    // console.log(confirmed);
     if(!confirmed){
         return 'Loading ...';
     }
