a625ca24b3b7107b23de6923348c750059750cf2
Mudit Mittal <mmudit30@gmail.com>
2020-05-29 21:18:35 +0530

    added daily data fetch function

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,26 @@
 import axios from 'axios';
 
-const url = 'https://covid19.mathdro.id/api/';
+const url = 'https://covid19.mathdro.id/api';
 
 export const fetchData = async () => {
     try{
         const {data : {recovered, deaths, confirmed, lastUpdate}}  = await axios.get(url);
         return {recovered, deaths, confirmed, lastUpdate};
     }
-    catch(error){
-        
+    catch(error){        
+    }
+}
+
+export const fetchDailyData = async () => {
+    try{
+        const { data }  = await axios.get(`${url}/daily`);
+        const modifiedData = data.map((dailyData) => ({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate
+        }));
+        return modifiedData;
+    }
+    catch(error){        
     }
 }
