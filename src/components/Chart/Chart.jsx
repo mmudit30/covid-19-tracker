72c3e04a50a860fe69f99025229389fce735744c
Mudit Mittal <mmudit30@gmail.com>
2020-05-29 21:18:54 +0530

    added line chart

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchDailyData } from '../../api';
+import { Bar, Line } from 'react-chartjs-2';
+import styles from './Chart.module.css';
 
 const Chart = () => {
+
+    const [dailyData, setDailyData]= useState([]);
+
+    useEffect(() => {
+        if(!dailyData.length){
+            const fetchApi = async() => {
+                setDailyData(await fetchDailyData());
+            }
+            // console.log(dailyData);            
+            fetchApi();
+        }
+    }, [dailyData]);
+
+    const Linechart = ()=>
+        (
+        dailyData.length ?
+        (<Line
+         data={
+            {
+                labels: dailyData.map(({date}) => date),
+                datasets: [{
+                    data: dailyData.map(({confirmed}) => confirmed),
+                    label: 'Infected',
+                    borderColor: 'rgba(0, 0, 255, 0.5)',
+                    fill: true
+                }, {
+                    data: dailyData.map(({deaths}) => deaths),
+                    label: 'Deaths',
+                    borderColor: 'rgba(255, 0, 0, 0.5)',
+                    backgroundColor: 'rgba(255, 0, 0, 0.5)',
+                    fill: true
+                }],
+            }
+         }
+         ></Line> ) : (<div></div>)
+        )
+
     return(
-        <div>
-            Chart
+        <div className={styles.container}>
+            <Linechart/>
         </div>
     );
 };
